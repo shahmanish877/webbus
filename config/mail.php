1c2bd7e087c9770803614dcbf2895efc2305e056
Manish Shah <shahmanish877@gmail.com>
2021-04-28 11:22:52 +0545

    email update with verification

--- config/mail.php
+++ config/mail.php
@@ -4,8 +4,8 @@ return [
   "host" => "smtp.mailtrap.io",
   "port" => 2525,
   "from" => array(
-      "address" => "from@example.com",
-      "name" => "Example"
+      "address" => "no-reply@yatribus.com.np",
+      "name" => "No reply"
   ),
   "username" => "15f766b97902f0",
   "password" => "37001701f2d46e",
